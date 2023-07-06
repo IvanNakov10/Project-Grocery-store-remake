d5d94684a1ce9580145b799d0d24851502c71c15
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-07-06 13:50:23 +0300

    reader

--- Grocery store/Grocery store/Item.cs
+++ Grocery store/Grocery store/Item.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Grocery_store
 {
     internal class Item
     {
+        private int productId;
+        private string name;
+        private string cayegory;
+        private double price;
+        private int quantity;
+
+        public Item(int productId, string name, string cayegory, double price, int quantity)
+        {
+            this.productId = productId;
+            this.name = name;
+            this.cayegory = cayegory;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public int ProductId { get => productId; set => productId = value; }
+        public string Name { get => name; set => name = value; }
+        public string Cayegory { get => cayegory; set => cayegory = value; }
+        public double Price { get => price; set => price = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
     }
 }
--- Grocery store/Grocery store/Program.cs
+++ Grocery store/Grocery store/Program.cs
@@ -5,14 +5,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Grocery_store
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            StreamReader streamReader = new StreamReader("Products.txt")
+            Reader();
 
+
+        }
+
+        static void Reader()
+        {
+            StreamReader streamReader = new StreamReader("Products.txt");
+
+            List<Item> items = new List<Item>();
+
+            string line = streamReader.ReadLine();
+
+            while (line != null)
+            {
+                List<string> itemInfo = line.Split(',').ToList();
+
+                Item current = new Item(int.Parse(itemInfo[0]), itemInfo[1], itemInfo[2], double.Parse(itemInfo[3]), int.Parse(itemInfo[4]));
+                
+                items.Add(current);
+
+                line = streamReader.ReadLine();
+            }
+            streamReader.Close();
         }
     }
 }
