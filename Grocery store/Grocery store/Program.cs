fa42b3656777e5c622b30726785c08c19a17047c
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-07-06 13:37:22 +0300

    strart file +products.casdwda

---
+++ Grocery store/Grocery store/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery_store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader("Products.txt")
+
+        }
+    }
+}
